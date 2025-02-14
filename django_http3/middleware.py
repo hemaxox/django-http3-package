209047e19725be6846e4ba9b5ada898c1a58c4a1
Ibrahim Muhaisen <ibra_him90@hotmail.com>
2025-02-14 21:19:55 +0100

    Add conditional Alt-Svc header in HTTP3Middleware based on settings
    This commit alters the HTTP3 middleware by adding functionality that checks the settings for both HTTP3_HOST and HTTP3_PORT. The middleware now only adds the Alt-Svc header if those settings exist, creating more room for flexibility while preventing misconfigurations.

--- django_http3/middleware.py
+++ django_http3/middleware.py
@@ -3,8 +3,11 @@ from django.conf import settings
 class HTTP3Middleware:
     def __init__(self, get_response):
         self.get_response = get_response
+        self.host = getattr(settings, 'HTTP3_HOST', None)
+        self.port = getattr(settings, 'HTTP3_PORT', None)
 
     def __call__(self, request):
         response = self.get_response(request)
-        response["Alt-Svc"] = f'h3="{settings.HTTP3_HOST}:{settings.HTTP3_PORT}"; ma=3600'
+        if self.host and self.port:
+            response["Alt-Svc"] = f'h3="{self.host}:{self.port}"; ma=3600'
         return response
